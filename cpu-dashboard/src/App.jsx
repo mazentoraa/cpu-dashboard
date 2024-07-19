dabd7205555e162495097eddaea64957cca0098d
Mazen Toraa <mazentoraa2@gmail.com>
2024-07-19 20:17:50 +0200

    testt

--- cpu-dashboard/src/App.jsx
+++ cpu-dashboard/src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import './App.css'
 import Dashboard from './Components/Dashboard';
 import Login from './Components/Login';
-
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 function App() {
   const [connected, setConnected] = useState(false)
