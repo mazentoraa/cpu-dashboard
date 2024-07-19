f4a7c87b85a15bc564693b55a8e4da405e49b260
Mazen Toraa <mazentoraa2@gmail.com>
2024-07-19 14:59:53 +0200

    Test

--- cpu-dashboard/src/App.jsx
+++ cpu-dashboard/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Dashboard from './Components/Dashboard';
 import Login from './Components/Login';
 
+
 function App() {
   const [connected, setConnected] = useState(false)
 
